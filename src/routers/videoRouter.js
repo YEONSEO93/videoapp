7ae71eec75f9e9f9e13a4b334b4f6ea6c5609a23
Yeonseo Ko <yeooonseo@gmail.com>
2024-11-06 16:21:44 +1000

    first commit_ routing, controller

---
+++ src/routers/videoRouter.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { watch, getEdit, postEdit, getUpload, postUpload } from "../controllers/videoController";
+
+const videoRouter = express.Router();
+
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+// videoRouter.get("/:id(\\d+)/edit", getEdit);
+// videoRouter.post("/:id(\\d+)/edit", postEdit);
+videoRouter.get("/upload", getUpload);
+videoRouter.post("/upload", postUpload);
+
+export default videoRouter;
