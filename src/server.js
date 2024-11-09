409d764f0334bce286cb2088bd5339ec17aede93
Yeonseo Ko <yeooonseo@gmail.com>
2024-11-09 14:39:21 +1000

    Auth_Github

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -17,9 +18,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    //   cookie: {
+    //   maxAge: 20000,
+    // },
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
