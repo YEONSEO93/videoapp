619c2afa021d682301bfd5749305853692f19096
Yeonseo Ko <yeooonseo@gmail.com>
2024-11-27 11:31:49 +1000

    Webpack

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
@@ -15,37 +14,19 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
-
 app.use(
   session({
     secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
-    //   cookie: {
-    //   maxAge: 20000,
-    // },
     store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
-
-app.use((req, res, next) => {
-  req.sessionStore.all((error, sessions) => {
-    console.log(sessions);
-    next();
-  });
-});
-
-app.get("/add-one", (req, res, next) => {
-  return res.send(`${req.session.id}`);
-});
-
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 export default app;
-
-
-//npm run dev
